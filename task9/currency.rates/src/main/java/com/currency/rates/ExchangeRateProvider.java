546cb6b9041181ee004ace83530ca41d7f8b3186
andirahim <burcifiqi@gamil.com>
2025-04-03 21:01:40 +0300

    commit #1
    in the nv on line 1 Defines the package for exchange rate management, on line 2  Import HashMap for storing exchange rates is added on line 3 Import Map interface is added on line 4 Class responsible for managing exchange rates is added on line 5 Static map to store exchange rates on line 6 Static block initializes exchange rates when the class loads is added on line 7,8,9 and 10 from USD to EUR from EUR to USD from USD to GBP from  GBP to USD conversion rate is added
    respectively

---
+++ task9/currency.rates/src/main/java/com/currency/rates/ExchangeRateProvider.java
@@ -0,0 +1,11 @@
+package com.currency.rates;
+import java.util.HashMap;
+import java.util.Map;
+public class ExchangeRateProvider {
+    private static final Map<String, Double> exchangeRates = new HashMap<>();
+    static {
+        exchangeRates.put("USD_EUR", 0.85);
+        exchangeRates.put("EUR_USD", 1.18);
+        exchangeRates.put("USD_GBP", 0.75);
+        exchangeRates.put("GBP_USD", 1.33);
+    }
